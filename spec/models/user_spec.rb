bb2a63522d57347fd9f5338a3a94cba5f0ebf222
Sergio Urrutia-Oyer <urrutise@oregonstate.edu>
2021-02-08 20:25:35 -0800

    check user object init, user returns email, and new user roles to default to organization

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+  it 'exists' do
+    User.new
+  end
+
+  it 'email returns email' do
+    user = User.new
+    expect(user).to respond_to(:email)
+  end
+
+  it 'check default role on init' do
+    user = User.new
+    expect(user.role).to eq("organization")
+  end
+
 end
