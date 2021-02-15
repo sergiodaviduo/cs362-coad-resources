8581f0c11a29865a1f77b6d2b4ffe9c8c7589257
Sergio Urrutia-Oyer <urrutise@oregonstate.edu>
2021-02-15 00:24:25 -0800

    Organization: approve and reject verify, default status

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -6,5 +6,34 @@ RSpec.describe Organization, type: :model do
     Organization.new
   end
 
+  it 'has name' do
+    org = Organization.new
+    expect(org).to respond_to(:name)
+  end
+
+  it 'check rejected' do
+    org = Organization.new
+    org.reject
+    expect(org.status).to eq("rejected")
+  end
+
+  it 'check approved' do
+    org = Organization.new
+    org.approve
+    expect(org.status).to eq("approved")
+  end
+
+  it 'set default status' do
+    org = Organization.new
+    org.set_default_status
+    expect(org.status).to eq("submitted")
+  end
+
+  it 'check unchanged status' do
+    org = Organization.new
+    org.approve
+    org.set_default_status
+    expect(org.status).to eq("approved")
+  end
 
 end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
+
   it 'check init' do
     Ticket.new
   end
