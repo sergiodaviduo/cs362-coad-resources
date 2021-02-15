0148c45c9bd217f3f2b1b46431273f125e08b49b
Sergio Urrutia-Oyer <urrutise@oregonstate.edu>
2021-02-14 23:14:42 -0800

    init tests for ticket and organization

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
 
+  it 'check init' do
+    Organization.new
+  end
+
+
 end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
+  it 'check init' do
+    Ticket.new
+  end
 
 end
