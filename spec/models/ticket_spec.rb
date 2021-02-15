add4629a44236e6e648762e81d47a940e6e29c77
Sergio Urrutia-Oyer <urrutise@oregonstate.edu>
2021-02-15 00:36:10 -0800

    Ticket: has name, no org on init, open on init

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -6,4 +6,19 @@ RSpec.describe Ticket, type: :model do
     Ticket.new
   end
 
+  it 'has name' do
+    ticket = Ticket.new
+    expect(ticket).to respond_to(:name)
+  end
+
+  it 'no organization on init' do
+    ticket = Ticket.new
+    expect(ticket.captured?).to eq(FALSE)
+  end
+
+  it 'open on init' do
+    ticket = Ticket.new
+    expect(ticket.open?).to eq(TRUE)
+  end
+
 end
